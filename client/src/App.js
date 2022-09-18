4c278b75169672e277935671ed54a5f3ccb6f177
William Wang <william.wang1@uwaterloo.ca>
2022-09-18 06:01:59 -0400

    added final styling

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ function App() {
   const [prompt, setPrompt] = React.useState('')
   const [product, setProduct] = React.useState('Zephyrus G14')
 
-
   // Update prompt variable when handleChange is called
   const handleChange = (abc) => {
     setPrompt(abc.target.value)
@@ -40,65 +39,60 @@ function App() {
   // When handleSubmit is called, passes prompt to /api and then gets assigns response to data
   const handleSubmit = (abc) => {
     abc.preventDefault()
-    setData(null)
+    setData("Processing...")
     fetch(`/api?prompt=${prompt}&product=${product}&productTraits=${product_traits}&seller=${seller}&client=${client}&clientTraits=${client_traits}`)
       .then((res) => res.json())
       .then((data) => {
-        // setData(`${data.generations[0].text.slice(0, -1).replace("<br/>", "<br/><br/>")}`);
-        // setData(data.replace("\n", "\n\n"));
-        // console.log("Data typw: " + typeof data);
-        // console.log("Data text: " + data.generations[0].text);
-        // console.log("Data tyoe text: " + typeof data.generations[0].text);
-        // console.log("Data n: " + data.generations[0].text.replace("\n", "\n\n"));
-
         setData(data.generations[0].text.split('\n').map(str => <p>{str}</p>));
         data.text.split('\n').map(str => <p>{str}</p>)
       }
       )
+      window.scroll({ bottom: 0, left: 0,  behavior: 'smooth' });
   }
-  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Personalized Sales Pitch Generator</h1>
+    <div className="App" class="container-md text-white">
+      <div class="main">
+        <h1 class="mt-5">Personalized Sales Pitch Generator</h1>
         <form onSubmit={handleSubmit}>
-          <label>
-            Who are you: <br />
-            <textarea name="input-box" rows="5" cols="100" value={seller} onChange={handleChangeSeller} />
-          </label>
-          <br />
-          <label>
-            What do you want to sell: <br />
-            <textarea name="input-box" rows="5" cols="100" value={product} onChange={handleChangeProduct} />
-          </label>
-          <br />
-          <label>
-            Product traits: <br />
-            <textarea name="input-box" rows="5" cols="100" value={product_traits} onChange={handleChangeProductTraits} />
-          </label>
+          <div class="form-group bg-dark text-white mt-3">
+            <label > Who are you: </label>
+            <input type="text" class="form-control" name="input-box" value={seller} onChange={handleChangeSeller} />
+            <br />
+          </div>
+
+          <div class="form-group bg-dark text-white mt-1">
+            <label> What do you want to sell: </label>
+            <input type="text" class="form-control" name="input-box" value={product} onChange={handleChangeProduct} />
+            <br />
+          </div>
+
+          <div class="form-group mt-1">
+            <label>Product traits: </label>
+            <input type="text" class="form-control" name="input-box" value={product_traits} onChange={handleChangeProductTraits} />
+            <br />
+          </div>
+
+          <div class="form-group mt-1">
+            <label>Who are you selling to:</label>
+            <input type="text" class="form-control" name="input-box" value={client} onChange={handleChangeClient} />
+            <br />
+          </div>
+
+          <div class="form-group mt-1">
+            <label> Client traits:</label>
+            <input type="text" class="form-control" name="input-box" value={client_traits} onChange={handleChangeClientTraits} />
+            <br />
+          </div>
+
+          <button type="submit" class="btn btn-info">Submit</button>
           <br />
 
-          <label>
-            Who are you selling to: <br />
-            <textarea name="input-box" rows="5" cols="100" value={client} onChange={handleChangeClient} />
-          </label>
-          <br />
-          <label>
-            Client traits: <br />
-            <textarea name="input-box" rows="5" cols="100" value={client_traits} onChange={handleChangeClientTraits} />
-          </label>
-
-          <br />
-          <input type="submit" value="Submit" />
         </form>
-        <h1>Result:</h1>
-        <div>{!data ? 'Loading' : data}</div>
-
-        
-
-        {/* <p>Feedback? <a href="mailto:elaine@cohere.com" className="App-link">Send me an email</a>. For errors please include a screenshot.</p>
-        <p>Powered by <a href="https://cohere.ai/" className="App-link">Cohere</a></p> */}
-      </header>
+        <br />
+        <h1 class="mt-3">Result:</h1>
+        <p class="text-left mb-5">{!data ? 'Please enter a prompt' : data}</p>
+      </div>
     </div>
   )
 }
